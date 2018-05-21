c37bd377809e6c16838689fcd24aff871b3828c9
Ryan Nagle <rmnagle@gmail.com>
2018-05-21 13:58:30 -0500

    Misc. bug fixes (#1)
    * we need to use localhost:8080 for now so the option is unnecessary
    
    * reverse order of Write and WriteHeader to avoid duplicate header warning
    
    * add simple retry mechanism to use for obtaining ephemeral port num

--- cmd/tkns/webdriver/tokenfetcher.go
+++ cmd/tkns/webdriver/tokenfetcher.go
@@ -2,19 +2,26 @@ package webdriver
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/tebeka/selenium"
 	"github.com/tebeka/selenium/firefox"
 )
 
 func FetchToken(cg CredGenerator, RedirectURL string) error {
-	port, err := port()
-	if err != nil {
-		return err
-	}
+	var portNum int
+
+	Retry(5, time.Second/2, func() error {
+		var err error
+		portNum, err = port()
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 
 	var opts []selenium.ServiceOption
-	s, err := selenium.NewGeckoDriverService("geckodriver", port, opts...)
+	s, err := selenium.NewGeckoDriverService("geckodriver", portNum, opts...)
 	if err != nil {
 		return err
 	}
@@ -24,13 +31,32 @@ func FetchToken(cg CredGenerator, RedirectURL string) error {
 	caps.AddFirefox(firefox.Capabilities{
 		Args: []string{"-headless"},
 	})
-	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d", port))
+	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d", portNum))
 	if err != nil {
 		return err
 	}
 	defer wd.Quit()
 	td := TokenDriver{wd, RedirectURL}
 	return td.Approve(cg.GetCreds())
+}
+
+func Retry(tries int, sleep time.Duration, fn func() error) error {
+	if err := fn(); err != nil {
+		if s, ok := err.(stop); ok {
+			return s.error
+		}
+
+		if tries--; tries > 0 {
+			time.Sleep(sleep)
+			return Retry(tries, 2*sleep, fn)
+		}
+		return err
+	}
+	return nil
+}
+
+type stop struct {
+	error
 }
 
 type CredGenerator interface {
