ed0f2515e70d7cd12627e99da845cec76deb11bd
Ryan Nagle <rmnagle@gmail.com>
2018-06-07 11:54:03 -0500

    allow fetching tokens for contiguous blocks of benes

--- cmd/tkns/main.go
+++ cmd/tkns/main.go
@@ -52,10 +52,11 @@ func init() {
 }
 
 func main() {
-	var verbose bool
+	var verbose, random bool
 	var tknCount int
 	var numWorkers int
 	var driverStartPort int
+	flag.BoolVar(&random, "random", false, "Should we generate random beneficiary credentials?")
 	flag.BoolVar(&verbose, "v", false, "Enable for verbose logging")
 	flag.IntVar(&tknCount, "n", 1, "The number of tokens to generate")
 	flag.IntVar(&numWorkers, "w", 1, "The number of workers to use")
@@ -102,7 +103,7 @@ func main() {
 
 	// Spin up token generation jobs
 	// ------------------------------
-	jobs := generateCredentials(tknCount)
+	jobs := generateCredentials(tknCount, random)
 
 	var g errgroup.Group
 	for i := 0; i < numWorkers; i++ {
@@ -123,10 +124,13 @@ func main() {
 	}
 }
 
-func generateCredentials(tknCount int) chan [2]string {
+func generateCredentials(tknCount int, random bool) chan [2]string {
 	jobs := make(chan [2]string, tknCount)
 	defer close(jobs)
-	generator := &RandomCred{}
+	generator := &Cred{
+		Random: random,
+	}
+
 	for i := 0; i < tknCount; i++ {
 		u, p := generator.GetCreds()
 		jobs <- [2]string{u, p}
@@ -152,11 +156,20 @@ func buildWebServiceAndDriver(portNum int) (*selenium.Service, selenium.WebDrive
 	return s, wd
 }
 
-type RandomCred struct{}
+type Cred struct {
+	Idx    int
+	Random bool
+}
 
-func (rc *RandomCred) GetCreds() (username, password string) {
-	u := rand.Intn(BENE_MAX)
-	username = fmt.Sprintf("BBUser%05d", u)
-	password = fmt.Sprintf("PW%05d!", u)
+func (c *Cred) GetCreds() (username, password string) {
+	if c.Random {
+		u := rand.Intn(BENE_MAX)
+		username = fmt.Sprintf("User%05d", u)
+		password = fmt.Sprintf("PW%05d!", u)
+	} else {
+		username = fmt.Sprintf("User%05d", c.Idx)
+		password = fmt.Sprintf("PW%05d!", c.Idx)
+		c.Idx++
+	}
 	return
 }
